ac7569df25897088b05cb9f543385ef68e234ccc
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-08-26 22:12:23 +0200

    Added chapter02, chapter03, chapter04

---
+++ BeginningCSharpAndDotNet/Chapter04/Ch04Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ch04Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer:");
+            int myInt = Convert.ToInt32(Console.ReadLine());
+            bool isLessThen10 = myInt < 10;
+            bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+            Console.WriteLine($"Integer less then 10? {isLessThen10}");
+            Console.WriteLine($"Integer between 0 and 5? {isBetween0And5}");
+            Console.WriteLine($"Excatly on of the above is true? {isLessThen10 ^ isBetween0And5}");
+
+            string myString  = "The number is ";
+            myString += myInt < 10 ? "less then 10" : "greater then 10";
+
+            Console.WriteLine(myString);
+        }
+    }
+}
