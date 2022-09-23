9b80b79ef7a4ce2bc0e026725aeea554e3b583b7
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-09-23 23:24:39 +0200

    Added more projects.

---
+++ OOP/ShapesClassDiagram/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesClassDiagram
+{
+    public class Rectangle : Shape
+    {
+        protected double side1, side2;
+
+        public Rectangle(double x, double y) : base(x, y)
+        {
+        }
+
+        public override double Area()
+        {
+            return side1 * side2;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * (side1 + side2);
+        }
+    }
+}
