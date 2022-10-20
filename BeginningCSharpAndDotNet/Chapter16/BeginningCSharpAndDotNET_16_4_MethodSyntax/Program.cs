88d1379fcf418d874452a6bfbb9c837f0cff9d4e
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-10-20 22:22:01 +0200

    Adding chapter 16

---
+++ BeginningCSharpAndDotNet/Chapter16/BeginningCSharpAndDotNET_16_4_MethodSyntax/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small",
+"Ruiz", "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
+
+var queryResults = names.Select(x => x);
+
+Console.WriteLine("Names begging with S:");
+foreach (string name in queryResults)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine("Program finished, press Enter/Return to continue: ");
+
